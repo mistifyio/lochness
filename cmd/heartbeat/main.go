751957448c5ff67e552a141e91bfa11eb8980efb
Manuel Mendez <mmendez@omniti.com>
2014-12-11 18:10:54 -0500

    add heartbeat updates

--- cmd/heartbeat/main.go
+++ cmd/heartbeat/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	var interval = flag.Int("interval", 60, "resource update interval in seconds")
+	var interval = flag.Int("interval", 60, "heartbeat/resource update interval in seconds")
 	flag.Parse()
 
 	e := etcd.NewClient([]string{"http://127.0.0.1:4001"})
@@ -31,6 +31,11 @@ func main() {
 		if err = hv.UpdateResources(); err != nil {
 			log.Println(err)
 		}
+		if err = hv.Heartbeat(time.Duration(*interval * 2)); err != nil {
+			log.Println(err)
+		}
+		os.Stdout.WriteString("♥ ")
+		os.Stdout.Sync()
 		time.Sleep(time.Duration(*interval) * time.Second)
 	}
 }
