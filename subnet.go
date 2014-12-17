36bf6208ff2279b37d1b964c71deb66fef6d1834
Brian Akins <bakins@omniti.com>
2014-12-17 16:39:43 -0500

    load addresses at same time as subnet

--- subnet.go
+++ subnet.go
@@ -2,6 +2,8 @@ package lochness
 
 import (
 	"encoding/json"
+	"errors"
+	"math/rand"
 	"net"
 	"path/filepath"
 
@@ -26,6 +28,7 @@ type (
 		CIDR          *net.IPNet        `json:"cidr"`
 		StartRange    net.IP            `json:"start"` // first usable IP in range
 		EndRange      net.IP            `json:"end"`   // last usable IP in range
+		addresses     map[uint32]string //all allocated addresses. use int as its quickest to go back and forth
 	}
 
 	// Subnets is a helper for slices of subnets
@@ -85,24 +88,29 @@ func (t *Subnet) UnmarshalJSON(input []byte) error {
 
 }
 
-// NewSubnet creates a new "blank" subnet.  Fill in the needed values and then call Save
-func (c *Context) NewSubnet() *Subnet {
+func (c *Context) blankSubnet(id string) *Subnet {
 	t := &Subnet{
-		context:  c,
-		ID:       uuid.New(),
-		Metadata: make(map[string]string),
+		context:   c,
+		ID:        id,
+		Metadata:  make(map[string]string),
+		addresses: make(map[uint32]string),
+	}
+
+	if id == "" {
+		t.ID = uuid.New()
 	}
 
 	return t
 }
 
+// NewSubnet creates a new "blank" subnet.  Fill in the needed values and then call Save
+func (c *Context) NewSubnet() *Subnet {
+	return c.blankSubnet("")
+}
+
 // Subnet fetches a single subnet by ID
 func (c *Context) Subnet(id string) (*Subnet, error) {
-	t := &Subnet{
-		context: c,
-		ID:      id,
-	}
-
+	t := c.blankSubnet(id)
 	err := t.Refresh()
 	if err != nil {
 		return nil, err
@@ -114,25 +122,35 @@ func (t *Subnet) key() string {
 	return filepath.Join(SubnetPath, t.ID, "metadata")
 }
 
-func (t *Subnet) fromResponse(resp *etcd.Response) error {
-	t.modifiedIndex = resp.Node.ModifiedIndex
-	return json.Unmarshal([]byte(resp.Node.Value), &t)
-}
-
 // Refresh reloads from the data store
 func (t *Subnet) Refresh() error {
-	resp, err := t.context.etcd.Get(t.key(), false, false)
+	resp, err := t.context.etcd.Get(filepath.Join(SubnetPath, t.ID), false, true)
 
 	if err != nil {
 		return err
 	}
 
-	if resp == nil || resp.Node == nil {
-		// should this be an error??
-		return nil
+	for _, n := range resp.Node.Nodes {
+		key := filepath.Base(n.Key)
+		switch key {
+
+		case "metadata":
+			if err := json.Unmarshal([]byte(n.Value), &t); err != nil {
+				return err
+			}
+			t.modifiedIndex = n.ModifiedIndex
+
+		case "addresses":
+			for _, n := range n.Nodes {
+				if ip := net.ParseIP(filepath.Base(n.Key)); ip != nil {
+					// just skip on error
+					t.addresses[ipToI32(ip)] = n.Value
+				}
+			}
+		}
 	}
 
-	return t.fromResponse(resp)
+	return nil
 }
 
 // Validate ensures the values are reasonable. It currently does nothing
@@ -178,17 +196,15 @@ func (t *Subnet) Addresses() (map[string]string, error) {
 
 	addresses := make(map[string]string)
 
-	resp, err := t.context.etcd.Get(t.addressKey(""), true, true)
-	if err != nil {
-		if IsKeyNotFound(err) {
-			return addresses, nil
+	start := ipToI32(t.StartRange)
+	end := ipToI32(t.EndRange)
+
+	// this is a horrible way to do this. should this be simple set math?
+	for i := start; i <= end; i++ {
+		if id, ok := t.addresses[i]; ok {
+			ip := i32ToIP(i)
+			addresses[ip.String()] = id
 		}
-
-		return nil, err
-	}
-
-	for _, n := range resp.Node.Nodes {
-		addresses[filepath.Base(n.Key)] = n.Value
 	}
 
 	return addresses, nil
@@ -204,41 +220,67 @@ func i32ToIP(a uint32) net.IP {
 	return net.IPv4(byte(a>>24), byte(a>>16), byte(a>>8), byte(a))
 }
 
+// AvailibleAddresses returns the availible ip addresses.
+// this is probably a horrible idea for ipv6
+func (t *Subnet) AvailibleAddresses() []net.IP {
+	addresses := make([]net.IP, 0, 0)
+	start := ipToI32(t.StartRange)
+	end := ipToI32(t.EndRange)
+
+	// this is a horrible way to do this. should this be simple set math?
+	for i := start; i <= end; i++ {
+		if _, ok := t.addresses[i]; !ok {
+			addresses = append(addresses, i32ToIP(i))
+		}
+	}
+
+	return addresses
+}
+
+func randomizeAddresses(a []net.IP) []net.IP {
+	for i := range a {
+		j := rand.Intn(i + 1)
+		a[i], a[j] = a[j], a[i]
+	}
+
+	return a
+}
+
 // ReserveAddress reserves an ip address. The id is guest id
 func (t *Subnet) ReserveAddress(id string) (net.IP, error) {
 
 	// hacky...
-
 	//should this lock?? or do we assume lock is held?
-	addresses, err := t.Addresses()
-	if err != nil {
-		return nil, err
+
+	avail := t.AvailibleAddresses()
+
+	if len(avail) == 0 {
+		return nil, errors.New("no availible addresses")
 	}
 
+	avail = randomizeAddresses(avail)
+
 	var chosen net.IP
-	start := ipToI32(t.StartRange)
-	end := ipToI32(t.EndRange)
-
-	// this assumes start and end are actually in the ipnet
-	for i := start; i <= end; i++ {
-		ip := i32ToIP(i)
+	for _, ip := range avail {
 		v := ip.String()
-		if _, ok := addresses[v]; !ok {
-
-			_, err = t.context.etcd.Create(t.addressKey(v), id, 0)
-			if err == nil {
-				chosen = ip
-				break
-			}
-
+		_, err := t.context.etcd.Create(t.addressKey(v), id, 0)
+		if err == nil {
+			chosen = ip
+			t.addresses[ipToI32(ip)] = id
+			break
 		}
 	}
 
+	// what is nothing was chosen?
 	return chosen, nil
 }
 
 // ReleaseAddress releases an address
 func (t *Subnet) ReleaseAddress(ip net.IP) error {
+
 	_, err := t.context.etcd.Delete(t.addressKey(ip.String()), false)
+	if err != nil {
+		delete(t.addresses, ipToI32(ip))
+	}
 	return err
 }
