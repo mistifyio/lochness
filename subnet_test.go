bd4436625d3133a3ca40f5ba5473531e5ae0ef1e
Brian Akins <bakins@omniti.com>
2014-12-18 11:45:31 -0500

    move test to context_test

--- subnet_test.go
+++ subnet_test.go
@@ -6,20 +6,8 @@ import (
 	"testing"
 
 	h "github.com/bakins/test-helpers"
-	"github.com/coreos/go-etcd/etcd"
 	"github.com/mistifyio/lochness"
 )
-
-func newContext(t *testing.T) *lochness.Context {
-	e := etcd.NewClient([]string{"http://127.0.0.1:4001"})
-	if !e.SyncCluster() {
-		t.Fatal("cannot sync cluster. make sure etcd is running at http://127.0.0.1:4001")
-	}
-
-	c := lochness.NewContext(e)
-
-	return c
-}
 
 func TestNewSubnet(t *testing.T) {
 	c := newContext(t)
