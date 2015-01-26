2d5da18dce06f2e0b1adff0f713f14bec3809741
Brian Akins <bakins@omniti.com>
2015-01-26 14:22:56 -0500

    add simple metrics

---
+++ cmd/enfield/main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"encoding/json"
+	_ "expvar"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"strings"
+	"text/template"
+
+	"github.com/armon/go-metrics"
+	"github.com/bakins/go-metrics-map"
+	"github.com/bakins/go-metrics-middleware"
+	"github.com/bakins/net-http-recover"
+	"github.com/coreos/go-etcd/etcd"
+	"github.com/gorilla/context"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/justinas/alice"
+	"github.com/mistifyio/lochness"
+)
+
+type Server struct {
+	ctx            *lochness.Context
+	t              *template.Template
+	defaultVersion string
+	baseUrl        string
+}
+
+const ipxeTemplate = `#!ipxe
+kernel {{.BaseUrl}}/images/{{.Version}}/vmlinuz {{.Options}}
+initrd {{.BaseUrl}}/images/{{.Version}}/initrd
+boot
+`
+
+func main() {
+	address := flag.String("port", ":8888", "address to listen")
+	eaddr := flag.String("etcd", "http://localhost:4001", "address of etcd machine")
+	baseUrl := flag.String("base", "http://ipxe.mistify.local:8888", "base address of bits request")
+	defaultVersion := flag.String("version", "0.1.0", "If all else fails, what version to serve")
+	imageDir := flag.String("images", "/var/lib/images", "directory containing the images")
+	flag.Parse()
+
+	e := etcd.NewClient([]string{*eaddr})
+	c := lochness.NewContext(e)
+
+	router := mux.NewRouter()
+	router.StrictSlash(true)
+
+	s := &Server{
+		ctx:            c,
+		t:              template.Must(template.New("ipxe").Parse(ipxeTemplate)),
+		defaultVersion: *defaultVersion,
+		baseUrl:        *baseUrl,
+	}
+
+	chain := alice.New(
+		func(h http.Handler) http.Handler {
+			return recovery.Handler(os.Stderr, h, true)
+		},
+		func(h http.Handler) http.Handler {
+			return handlers.CombinedLoggingHandler(os.Stdout, h)
+		},
+		handlers.CompressHandler,
+	)
+
+	sink := mapsink.New()
+	conf := metrics.DefaultConfig("enfield")
+	conf.EnableHostname = false
+	m, _ := metrics.New(conf, sink)
+	mw := mmw.New(m)
+
+	router.PathPrefix("/debug/").Handler(chain.Append(mw.HandlerWrapper("debug")).Then((http.DefaultServeMux)))
+	router.PathPrefix("/images").Handler(chain.Append(mw.HandlerWrapper("images")).Then(http.StripPrefix("/images/", http.FileServer(http.Dir(*imageDir)))))
+
+	router.Handle("/ipxe/{ip}", chain.Append(
+		func(h http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				context.Set(r, "_server_", s)
+				h.ServeHTTP(w, r)
+			})
+		},
+	).Append(mw.HandlerWrapper("ipxe")).ThenFunc(ipxeHandler))
+
+	router.Handle("/metrics", chain.Append(mw.HandlerWrapper("metrics")).ThenFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			json.NewEncoder(w).Encode(sink)
+		}))
+
+	log.Fatal(http.ListenAndServe(*address, router))
+}
+
+func ipxeHandler(w http.ResponseWriter, r *http.Request) {
+
+	s := context.Get(r, "_server_").(*Server)
+
+	ip := mux.Vars(r)["ip"]
+
+	if net.ParseIP(ip) == nil {
+		http.Error(w, "invalid address", http.StatusBadRequest)
+		return
+	}
+
+	found, err := s.ctx.FirstHypervisor(func(h *lochness.Hypervisor) bool {
+		return ip == h.IP.String()
+	})
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if found == nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	version := found.Config["version"]
+
+	if version == "" {
+		version, err = s.ctx.GetConfig("defaultVersion")
+		if err != nil && !lochness.IsKeyNotFound(err) {
+			// XXX: should be fatal?
+			log.Println(err)
+		}
+		if version == "" {
+			version = s.defaultVersion
+		}
+	}
+
+	options := map[string]string{
+		"uuid": found.ID,
+	}
+
+	data := map[string]string{
+		"BaseUrl": s.baseUrl,
+		"Options": mapToOptions(options),
+		"Version": version,
+	}
+	err = s.t.Execute(w, data)
+	if err != nil {
+		// we shouldn not get here
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func mapToOptions(m map[string]string) string {
+	var parts []string
+
+	for k, v := range m {
+		// need to sanitize ?
+		parts = append(parts, fmt.Sprintf("%s=%s", k, v))
+	}
+	return strings.Join(parts, " ")
+}
