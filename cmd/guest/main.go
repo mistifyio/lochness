2a3352d299b737a1896c1fe3fc6ad5c2dcc6721f
Brian Bickerton <bbickerton@omniti.com>
2015-02-04 21:47:15 +0000

    MIST-304 Read from stdin when appropriate

--- cmd/guest/main.go
+++ cmd/guest/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"os"
 	"sort"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/andrew-d/go-termutil"
 	"github.com/mistifyio/lochness/pkg/internal/cli"
 	"github.com/spf13/cobra"
 )
@@ -43,15 +45,19 @@ func deleteGuest(c *cli.Client, id string) cli.JMap {
 	return c.Del("hypervisor", "guests/"+id)
 }
 
-func list(cmd *cobra.Command, ids []string) {
+func list(cmd *cobra.Command, args []string) {
 	c := cli.NewClient(server)
 	guests := []cli.JMap{}
-
-	if len(ids) == 0 {
-		guests = getGuests(c)
-		sort.Sort(cli.JMapSlice(guests))
-	} else {
-		for _, id := range ids {
+	if len(args) == 0 {
+		if termutil.Isatty(os.Stdin.Fd()) {
+			guests = getGuests(c)
+			sort.Sort(cli.JMapSlice(guests))
+		} else {
+			args = cli.Read(os.Stdin)
+		}
+	}
+	if len(guests) == 0 {
+		for _, id := range args {
 			cli.AssertID(id)
 			guests = append(guests, getGuest(c, id))
 		}
@@ -64,6 +70,10 @@ func list(cmd *cobra.Command, ids []string) {
 
 func create(cmd *cobra.Command, specs []string) {
 	c := cli.NewClient(server)
+	if len(specs) == 0 {
+		specs = cli.Read(os.Stdin)
+	}
+
 	for _, spec := range specs {
 		cli.AssertSpec(spec)
 		guest := createGuest(c, spec)
@@ -73,9 +83,13 @@ func create(cmd *cobra.Command, specs []string) {
 
 func modify(cmd *cobra.Command, args []string) {
 	c := cli.NewClient(server)
+	if len(args) == 0 {
+		args = cli.Read(os.Stdin)
+	}
 	if len(args)%2 != 0 {
 		log.WithField("num", len(args)).Fatal("expected an even number of args")
 	}
+
 	for i := 0; i < len(args); i += 2 {
 		id := args[i]
 		cli.AssertID(id)
@@ -89,6 +103,10 @@ func modify(cmd *cobra.Command, args []string) {
 
 func del(cmd *cobra.Command, ids []string) {
 	c := cli.NewClient(server)
+	if len(ids) == 0 {
+		ids = cli.Read(os.Stdin)
+	}
+
 	for _, id := range ids {
 		cli.AssertID(id)
 		guest := deleteGuest(c, id)
@@ -98,9 +116,9 @@ func del(cmd *cobra.Command, ids []string) {
 
 func main() {
 	root := &cobra.Command{
-		Use:   "guest",
-		Short: "guest is the cli interface to waheela",
-		Run:   help,
+		Use:  "guest",
+		Long: "guest is the cli interface to waheela. All commands support arguments via command line or stdin.",
+		Run:  help,
 	}
 	root.PersistentFlags().BoolVarP(&jsonout, "jsonout", "j", jsonout, "output in json")
 	root.PersistentFlags().StringVarP(&server, "server", "s", server, "server address to connect to")
