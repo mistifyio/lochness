ad1fe0aa5a0c8452825b2f87b5d40dc97b428015
Manuel Mendez <mmendez@omniti.com>
2015-02-11 11:30:07 -0500

    cmd/cherufe: deal with nft -f weirdness.
    nft will not process vmap definitinos within chain definitions, so we
    must add rules with the vmap specs after the chains are setup/defined
    
    MIST-336

--- cmd/cherufe/nftables.ego.go
+++ cmd/cherufe/nftables.ego.go
@@ -8,7 +8,7 @@ import (
 //line nftables.ego:1
 func nftWrite(w io.Writer, ip string, groups groupMap, guests guestMap) error {
 //line nftables.ego:2
-	_, _ = fmt.Fprintf(w, "\nflush ruleset\n\ntable inet filter {\n  ")
+	_, _ = fmt.Fprintf(w, "\nflush ruleset\n\ntable ip filter {\n  ")
 //line nftables.ego:5
 	for id, fwg := range groups {
 //line nftables.ego:6
@@ -60,34 +60,34 @@ func nftWrite(w io.Writer, ip string, groups groupMap, guests guestMap) error {
 //line nftables.ego:16
 	}
 //line nftables.ego:17
-	_, _ = fmt.Fprintf(w, "\n  chain input {\n    type filter hook input priority 0;\n\n    # allow established/related connections\n    ct state {established, related} accept\n\n    # early drop of invalid connections\n    ct state invalid drop\n\n    # allow from loopback\n    iifname lo accept\n\n    # allow icmp\n    ip protocol icmp accept\n    ip6 nexthdr icmpv6 accept\n\n    # allow lochness hv traffic\n    ip daddr ")
-//line nftables.ego:34
+	_, _ = fmt.Fprintf(w, "\n  chain input {\n    type filter hook input priority 0;\n\n    # allow established/related connections\n    ct state {established, related} accept\n\n    # early drop of invalid connections\n    ct state invalid drop\n\n    # allow from loopback\n    iifname lo accept\n\n    # allow icmp\n    ip protocol icmp accept\n\n    # allow lochness hv traffic\n    ip daddr ")
+//line nftables.ego:33
 	_, _ = fmt.Fprintf(w, "%v", ip)
-//line nftables.ego:34
-	_, _ = fmt.Fprintf(w, " accept\n\n    ")
-//line nftables.ego:36
+//line nftables.ego:33
+	_, _ = fmt.Fprintf(w, " accept\n\n  }\n\n  chain forward {\n    type filter hook forward priority 0;\n    drop\n  }\n\n  chain output {\n    type filter hook output priority 0;\n  }\n}\n\n")
+//line nftables.ego:47
 	if len(guests) > 0 {
-//line nftables.ego:37
-		_, _ = fmt.Fprintf(w, "\n    # Allow traffic to guests as specified by FWGroups\n    ip daddr vmap { ")
-//line nftables.ego:38
+//line nftables.ego:48
+		_, _ = fmt.Fprintf(w, "\n# Allow traffic to guests as specified by FWGroups\nadd rule filter input ip daddr vmap { ")
+//line nftables.ego:49
 		for ip, fwgIndex := range guests {
-//line nftables.ego:39
-			_, _ = fmt.Fprintf(w, "\n\t    ")
-//line nftables.ego:39
+//line nftables.ego:50
+			_, _ = fmt.Fprintf(w, "\n    ")
+//line nftables.ego:50
 			_, _ = fmt.Fprintf(w, "%v", ip)
-//line nftables.ego:39
+//line nftables.ego:50
 			_, _ = fmt.Fprintf(w, " : jump g")
-//line nftables.ego:39
+//line nftables.ego:50
 			_, _ = fmt.Fprintf(w, "%v", fwgIndex)
-//line nftables.ego:39
+//line nftables.ego:50
 			_, _ = fmt.Fprintf(w, ", ")
-//line nftables.ego:39
+//line nftables.ego:50
 		}
-//line nftables.ego:40
-		_, _ = fmt.Fprintf(w, "\n    }\n    ")
-//line nftables.ego:41
+//line nftables.ego:51
+		_, _ = fmt.Fprintf(w, "\n}\n")
+//line nftables.ego:52
 	}
-//line nftables.ego:42
-	_, _ = fmt.Fprintf(w, "\n    # everything else\n    reject with icmp type port-unreachable\n  }\n\n  chain forward {\n    type filter hook forward priority 0;\n    drop\n  }\n\n  chain output {\n    type filter hook output priority 0;\n  }\n}\n")
+//line nftables.ego:53
+	_, _ = fmt.Fprintf(w, "\n\n# everything else\nadd rule filter input reject with icmp type port-unreachable\n")
 	return nil
 }
