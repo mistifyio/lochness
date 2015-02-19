b0dd90781283624ca7a3459422812085e6918b1a
Brian Bickerton <bbickerton@omniti.com>
2015-02-19 19:07:02 +0000

    MIST-369 Add ansible running with tag support

--- cmd/kappa/main.go
+++ cmd/kappa/main.go
@@ -5,7 +5,10 @@ import (
 	"errors"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"os/signal"
+	"path"
+	"strings"
 	"syscall"
 
 	log "github.com/Sirupsen/logrus"
@@ -13,16 +16,28 @@ import (
 	flag "github.com/ogier/pflag"
 )
 
+type (
+	KeyOpts struct {
+		Tags      []string
+		Recursive bool
+	}
+
+	Config map[string]KeyOpts
+)
+
+var ansibleDir = "/root/lochness-ansible"
+var config Config
+
 // loadConfig reads the config file and unmarshals it into a map containing
 // keys to watch and whether to do so recursively. The config file should not
 // be empty
-func loadConfig(path string) (map[string]bool, error) {
+func loadConfig(path string) (Config, error) {
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
 
-	config := make(map[string]bool)
+	config := make(Config)
 	if err := json.Unmarshal(data, &config); err != nil {
 		return nil, err
 	}
@@ -32,6 +47,50 @@ func loadConfig(path string) (map[string]bool, error) {
 	}
 
 	return config, nil
+}
+
+// getTags returns the ansible tags, if any, associated with a key
+func getTags(key string) []string {
+	// Check for exact match (non-recursive watch)
+	keyOpts, ok := config[key]
+	if ok {
+		return keyOpts.Tags
+	}
+
+	// Check for prefix (recursive watch)
+	for watchKey, keyOpts := range config {
+		if !strings.HasPrefix(key, watchKey) {
+			continue
+		}
+		return keyOpts.Tags
+	}
+
+	return nil
+}
+
+// runAnsible kicks off an ansible run
+func runAnsible(key string) {
+	keyTags := getTags(key)
+	log.Info("TAGS:", keyTags)
+	args := make([]string, 0, len(keyTags)+1)
+	if len(keyTags) > 0 {
+		args = append(args, "-t")
+		args = append(args, keyTags...)
+	}
+	cmd := exec.Command(path.Join(ansibleDir, "run"), args...)
+	cmd.Dir = ansibleDir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.WithFields(log.Fields{
+			"key":        key,
+			"ansibleDir": ansibleDir,
+			"args":       args,
+			"error":      err,
+			"errorMsg":   err.Error(),
+		}).Fatal("ansible run failed")
+	}
 }
 
 // handleEvents handles notifications from etcd.Watch
@@ -44,13 +103,15 @@ func handleEvents(receiver chan *etcd.Response, done chan struct{}) {
 			return
 		case resp := <-receiver:
 			log.WithField("response", resp).Info("response received")
-			// TODO: Run ansible, either a full run or specific tag
+			runAnsible(resp.Node.Key)
+			log.WithField("response", resp).Info("response processed")
 		}
 	}
 }
 
 func main() {
 	logLevel := flag.StringP("log-level", "l", "warn", "log level")
+	flag.StringVarP(&ansibleDir, "ansible", "a", ansibleDir, "directory containing the ansible run command")
 	eaddr := flag.StringP("etcd", "e", "http://127.0.0.1:4001", "address of etcd server")
 	configPath := flag.StringP("config", "c", "", "path to config file with keys")
 	flag.Parse()
@@ -68,7 +129,7 @@ func main() {
 	log.SetLevel(level)
 
 	// Load config containing keys to watch
-	config, err := loadConfig(*configPath)
+	config, err = loadConfig(*configPath)
 	if err != nil {
 		log.WithFields(log.Fields{
 			"error":      err,
@@ -98,10 +159,20 @@ func main() {
 	go handleEvents(receiver, done)
 
 	// start watching etcd keys
-	for key, recursive := range config {
-		go etcdClient.Watch(key, 0, recursive, receiver, nil)
+	for key, keyOpts := range config {
+		go func() {
+			if _, err := etcdClient.Watch(key, 0, keyOpts.Recursive, receiver, nil); err != nil {
+				log.WithFields(log.Fields{
+					"key":      key,
+					"keyOpts":  keyOpts,
+					"error":    err,
+					"errorMsg": err.Error(),
+				}).Fatal("watch error")
+			}
+		}()
 	}
 
+	log.Info("ready")
 	// handle signals for clean shutdown
 	sigs := make(chan os.Signal)
 	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
