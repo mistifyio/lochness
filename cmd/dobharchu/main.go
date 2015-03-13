f8cccd0677a763ac197116315946f782f5587a20
Reha Sterbin <reha@omniti.com>
2015-03-13 16:22:28 +0000

    [MIST-370] Broke out the writing of config files into its own package with a test

--- cmd/dobharchu/main.go
+++ cmd/dobharchu/main.go
@@ -1,374 +1,48 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"strings"
+	"os"
 
 	log "github.com/Sirupsen/logrus"
-	"github.com/coreos/go-etcd/etcd"
-	"github.com/mistifyio/lochness"
+	"github.com/mistifyio/lochness/cmd/dobharchu/refresher"
+	logx "github.com/mistifyio/mistify-logrus-ext"
 	flag "github.com/spf13/pflag"
 )
-
-func doTestSetup(context *lochness.Context, etcdClient *etcd.Client) {
-
-	// Clear out the lochness keys
-	_, err := etcdClient.Delete("lochness/", true)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"error": err,
-			"func":  "etcd.Get",
-		}).Fatal("Could not clear data from etcd")
-	}
-
-	// Add flavors, network, and subnet
-	f1 := newTestFlavor(context, 4, 4096, 8192)
-	f2 := newTestFlavor(context, 6, 8192, 1024)
-	n := newTestNetwork(context)
-	fw := newTestFirewallGroup(context)
-	s := newTestSubnet(context, "10.10.10.0/24", net.IPv4(10, 10, 10, 1), net.IPv4(10, 10, 10, 10), net.IPv4(10, 10, 10, 250), n)
-
-	// Add hypervisors
-	h1 := newTestHypervisor(context, "de:ad:be:ef:7f:21", net.IPv4(192, 168, 100, 200), net.IPv4(192, 168, 100, 1), net.IPv4(255, 255, 255, 0), "br0", s)
-	h2 := newTestHypervisor(context, "de:ad:be:ef:7f:23", net.IPv4(192, 168, 100, 203), net.IPv4(192, 168, 100, 1), net.IPv4(255, 255, 255, 0), "br0", s)
-
-	// Add guests
-	_ = newTestGuest(context, "01:23:45:67:89:ab", n, s, f1, fw, h1)
-	_ = newTestGuest(context, "23:45:67:89:ab:cd", n, s, f2, fw, h1)
-	_ = newTestGuest(context, "45:67:89:ab:cd:ef", n, s, f1, fw, h2)
-	_ = newTestGuest(context, "67:89:ab:cd:ef:01", n, s, f2, fw, h2)
-}
-
-func newTestFlavor(context *lochness.Context, cpu uint32, memory, disk uint64) *lochness.Flavor {
-	f := context.NewFlavor()
-	f.CPU = cpu
-	f.Memory = memory
-	f.Disk = disk
-	if err := f.Save(); err != nil {
-		log.WithFields(log.Fields{
-			"func":  "Save",
-			"error": err,
-			"id":    f.ID,
-			"item":  "flavor",
-		}).Fatal("failed to save flavor")
-	}
-	return f
-}
-
-func newTestNetwork(context *lochness.Context) *lochness.Network {
-	n := context.NewNetwork()
-	if err := n.Save(); err != nil {
-		log.WithFields(log.Fields{
-			"func":  "Save",
-			"error": err,
-			"id":    n.ID,
-			"item":  "network",
-		}).Fatal("failed to save network")
-	}
-	return n
-}
-
-func newTestFirewallGroup(context *lochness.Context) *lochness.FWGroup {
-	fw := context.NewFWGroup()
-	fw.Rules = append(fw.Rules, &lochness.FWRule{})
-	if err := fw.Save(); err != nil {
-		log.WithFields(log.Fields{
-			"func":  "Save",
-			"error": err,
-			"id":    fw.ID,
-			"item":  "fwgroup",
-		}).Fatal("failed to save fwgroup")
-	}
-	return fw
-}
-
-func newTestHypervisor(context *lochness.Context, mac string, ip, gateway, netmask net.IP, ifname string, s *lochness.Subnet) *lochness.Hypervisor {
-	var err error
-	h := context.NewHypervisor()
-	h.IP = ip
-	h.MAC, err = net.ParseMAC(mac)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"func":  "net.ParseMAC",
-			"error": err,
-			"id":    h.ID,
-			"mac":   mac,
-		}).Fatal("Could not parse hypervisor MAC")
-	}
-	h.Gateway = gateway
-	h.Netmask = netmask
-	if err = h.Save(); err != nil {
-		log.WithFields(log.Fields{
-			"func":  "lochness.Hypervisor.Save",
-			"error": err,
-			"id":    h.ID,
-		}).Fatal("Could not save hypervisor")
-	}
-	if err := h.AddSubnet(s, ifname); err != nil {
-		log.WithFields(log.Fields{
-			"func":  "AddSubnet",
-			"error": err,
-			"id":    h.ID,
-			"item":  "hypervisor",
-		}).Fatal("failed to add subnet")
-	}
-	return h
-}
-
-func newTestGuest(context *lochness.Context, mac string, n *lochness.Network, s *lochness.Subnet, f *lochness.Flavor, fw *lochness.FWGroup, h *lochness.Hypervisor) *lochness.Guest {
-	var err error
-	g := context.NewGuest()
-	g.MAC, err = net.ParseMAC(mac)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"func":  "net.ParseMAC",
-			"error": err,
-			"id":    g.ID,
-			"mac":   mac,
-		}).Fatal("Could not parse guest MAC")
-	}
-	g.NetworkID = n.ID
-	g.SubnetID = s.ID
-	g.FlavorID = f.ID
-	g.FWGroupID = fw.ID
-	if err := g.Save(); err != nil {
-		log.WithFields(log.Fields{
-			"func":  "Save",
-			"error": err,
-			"id":    g.ID,
-			"item":  "guest",
-		}).Fatal("failed to save guest")
-	}
-
-	if err := h.AddGuest(g); err != nil {
-		log.WithFields(log.Fields{
-			"func":  "AddGuest",
-			"error": err,
-			"id":    h.ID,
-			"item":  "hypervisor",
-			"guest": g.ID,
-		}).Fatal("failed to add guest to hypervisor")
-	}
-
-	g.IP, err = s.ReserveAddress(g.ID)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"func":  "ReserverAddress",
-			"error": err,
-			"id":    s.ID,
-			"item":  "subnet",
-		}).Fatal("failed to reserve address")
-	}
-
-	if err := g.Save(); err != nil {
-		log.WithFields(log.Fields{
-			"func":  "Save",
-			"error": err,
-			"id":    g.ID,
-			"item":  "guest",
-		}).Fatal("failed to save guest")
-	}
-
-	return g
-}
-
-func newTestSubnet(context *lochness.Context, cidr string, gateway, start, end net.IP, n *lochness.Network) *lochness.Subnet {
-	var err error
-	s := context.NewSubnet()
-	_, s.CIDR, err = net.ParseCIDR(cidr)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"func":  "net.ParseCIDR",
-			"error": err,
-			"id":    s.ID,
-			"cidr":  cidr,
-		}).Fatal("Could not parse subnet CIDR")
-	}
-	s.Gateway = gateway
-	s.StartRange = start
-	s.EndRange = end
-	if err := s.Save(); err != nil {
-		log.WithFields(log.Fields{
-			"func":  "Save",
-			"error": err,
-			"id":    s.ID,
-			"item":  "subnet",
-		}).Fatal("Could not save subnet")
-	}
-	if err := n.AddSubnet(s); err != nil {
-		log.WithFields(log.Fields{
-			"func":  "AddSubnet",
-			"error": err,
-			"id":    n.ID,
-			"item":  "network",
-		}).Fatal("failed to add subnet to network")
-	}
-	return s
-}
-
-func fetchHypervisors(context *lochness.Context, etcdClient *etcd.Client) map[string]*lochness.Hypervisor {
-	res, err := etcdClient.Get("lochness/hypervisors/", true, true)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"error": err,
-			"func":  "etcd.Get",
-		}).Fatal("Could not retrieve hypervisors from etcd")
-	}
-	hypervisors := make(map[string]*lochness.Hypervisor)
-	for _, node := range res.Node.Nodes {
-		for _, hnode := range node.Nodes {
-			if strings.Contains(hnode.Key, "metadata") {
-				hv := context.NewHypervisor()
-				hv.UnmarshalJSON([]byte(hnode.Value))
-				hypervisors[hv.ID] = hv
-			}
-		}
-	}
-	log.WithFields(log.Fields{
-		"hypervisors": hypervisors,
-	}).Info("Fetched hypervisors metadata")
-	return hypervisors
-}
-
-func fetchGuests(context *lochness.Context, etcdClient *etcd.Client) map[string]*lochness.Guest {
-	res, err := etcdClient.Get("lochness/guests/", true, true)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"error": err,
-			"func":  "etcd.Get",
-		}).Fatal("Could not retrieve guests from etcd")
-	}
-	guests := make(map[string]*lochness.Guest)
-	for _, node := range res.Node.Nodes {
-		for _, gnode := range node.Nodes {
-			if strings.Contains(gnode.Key, "metadata") {
-				g := context.NewGuest()
-				g.UnmarshalJSON([]byte(gnode.Value))
-				guests[g.ID] = g
-			}
-		}
-	}
-	log.WithFields(log.Fields{
-		"guests": guests,
-	}).Info("Fetched guests metadata")
-	return guests
-}
-
-func fetchSubnets(context *lochness.Context, etcdClient *etcd.Client) map[string]*lochness.Subnet {
-	res, err := etcdClient.Get("lochness/subnets/", true, true)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"error": err,
-			"func":  "etcd.Get",
-		}).Fatal("Could not retrieve subnets from etcd")
-	}
-	subnets := make(map[string]*lochness.Subnet)
-	for _, node := range res.Node.Nodes {
-		for _, snode := range node.Nodes {
-			if strings.Contains(snode.Key, "metadata") {
-				s := context.NewSubnet()
-				s.UnmarshalJSON([]byte(snode.Value))
-				subnets[s.ID] = s
-			}
-		}
-	}
-	log.WithFields(log.Fields{
-		"subnets": subnets,
-	}).Info("Fetched subnets metadata")
-	return subnets
-}
-
-func writeGuestsConfigFile(domain string, guests map[string]*lochness.Guest, subnets map[string]*lochness.Subnet) {
-	fmt.Println("# Confd Generated")
-	fmt.Println("")
-	fmt.Println("group guests {")
-	fmt.Println("    option domain-name " + domain + ";")
-	fmt.Println("")
-	for _, g := range guests {
-		if g.HypervisorID == "" || g.SubnetID == "" {
-			continue
-		}
-		s, ok := subnets[g.SubnetID]
-		if !ok {
-			continue
-		}
-		fmt.Println("    host " + g.ID + " {")
-		fmt.Println("        hardware ethernet " + strings.ToUpper(g.MAC.String()) + ";")
-		fmt.Println("        fixed-address " + g.IP.String() + ";")
-		fmt.Println("        option routers " + s.Gateway.String() + ";")
-		fmt.Println("        option subnet-mask " + s.CIDR.IP.String() + ";")
-		fmt.Println("    }")
-		fmt.Println("")
-	}
-	fmt.Println("}")
-	fmt.Println("")
-}
-
-func writeHypervisorsConfigFile(domain string, hypervisors map[string]*lochness.Hypervisor) {
-	fmt.Println("# Confd Generated")
-	fmt.Println("")
-	fmt.Println("group hypervisors {")
-	fmt.Println("    option domain-name \"nodes." + domain + "\";")
-	fmt.Println("    if exists user-class and option user-class = \"iPXE\" {")
-	fmt.Println("        filename \"http://ipxe.services." + domain + ":8888/ipxe/${net0/ip}\";")
-	fmt.Println("    } else {")
-	fmt.Println("        next-server tftp.services." + domain + ";")
-	fmt.Println("        filename \"undionly.kpxe\";")
-	fmt.Println("    }")
-	fmt.Println("")
-	for _, hv := range hypervisors {
-		if hv.Gateway != nil && hv.Netmask != nil {
-			fmt.Println("    host " + hv.ID + " {")
-			fmt.Println("        hardware ethernet   " + strings.ToUpper(hv.MAC.String()) + ";")
-			fmt.Println("        fixed-address       " + hv.IP.String() + ";")
-			fmt.Println("        option routers      " + hv.Gateway.String() + ";")
-			fmt.Println("        option subnet-mask  " + hv.Netmask.String() + ";")
-			fmt.Println("    }")
-			fmt.Println("")
-		}
-	}
-	fmt.Println("}")
-	fmt.Println("")
-}
 
 func main() {
 
 	// Command line options
-	var doSetup bool
 	var etcdAddress, domain, logLevel string
 	flag.StringVarP(&etcdAddress, "etcd", "e", "http://127.0.0.1:4001", "address of etcd server")
 	flag.StringVarP(&domain, "domain", "d", "lochness.local", "domain for lochness")
 	flag.StringVarP(&logLevel, "log-level", "l", "warning", "log level: debug/info/warning/error/critical/fatal")
-	flag.BoolVarP(&doSetup, "setup", "s", false, "turn on to push a hypervisor into etcd first")
 	flag.Parse()
 
 	// Logging
-	log.SetFormatter(&log.JSONFormatter{})
-	level, err := log.ParseLevel(logLevel)
+	err := logx.DefaultSetup(logLevel)
 	if err != nil {
 		log.WithFields(log.Fields{
 			"error": err,
-			"func":  "log.ParseLevel",
-		}).Fatal("Could not parse log level")
-	}
-	log.SetLevel(level)
-
-	// Lochness Context
-	etcdClient := etcd.NewClient([]string{etcdAddress})
-	context := lochness.NewContext(etcdClient)
-
-	// Set up some hypervisors to work with
-	if doSetup {
-		doTestSetup(context, etcdClient)
+			"func":  "logx.DefaultSetup",
+		}).Fatal("Could not set up logrus")
 	}
 
-	// Fetch hypervisors, guests, and subnets
-	hypervisors := fetchHypervisors(context, etcdClient)
-	guests := fetchGuests(context, etcdClient)
-	subnets := fetchSubnets(context, etcdClient)
+	// Set up refresher
+	r := refresher.NewRefresher(domain, etcdAddress)
 
-	// Just print out the config for now
-	writeGuestsConfigFile(domain, guests, subnets)
-	writeHypervisorsConfigFile(domain, hypervisors)
+	// Just print out the configs for now
+	err = r.WriteHypervisorsConfigFile(os.Stdout) // eventual location: "/etc/dhcpd/hypervisors.conf"
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+			"func":  "refresher.WriteHypervisorsConfigFile",
+		}).Fatal("Could not write /etc/dhcpd/hypervisors.conf")
+	}
+	err = r.WriteGuestsConfigFile(os.Stdout) // eventual location: "/etc/dhcpd/guests.conf"
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+			"func":  "refresher.WriteGuestsConfigFile",
+		}).Fatal("Could not write /etc/dhcpd/guests.conf")
+	}
 }
---
+++ cmd/dobharchu/refresher/refresher_test.go
@@ -0,0 +1,561 @@
+package refresher_test
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	// "fmt"
+	"net"
+	"regexp"
+	"strings"
+	"testing"
+
+	h "github.com/bakins/test-helpers"
+	"github.com/coreos/go-etcd/etcd"
+	"github.com/mistifyio/lochness"
+	"github.com/mistifyio/lochness/cmd/dobharchu/refresher"
+)
+
+type (
+	TestingData struct {
+		hypervisors map[string]*TestingHypervisorData
+		guests      map[string]*TestingGuestData
+	}
+	TestingHypervisorData struct {
+		mac     string
+		ip      string
+		gateway string
+		netmask string
+	}
+	TestingGuestData struct {
+		mac     string
+		ip      string
+		gateway string
+		cidr    string
+	}
+)
+
+func doTestSetup(context *lochness.Context, etcdClient *etcd.Client) (*TestingData, error) {
+	data := &TestingData{
+		hypervisors: make(map[string]*TestingHypervisorData),
+		guests:      make(map[string]*TestingGuestData),
+	}
+
+	// Clear out the lochness keys
+	_, err := etcdClient.Delete("lochness/", true)
+	if err != nil {
+		return nil, errors.New("Could not clear data from etcd: " + err.Error())
+	}
+
+	// Add flavors, network, and firewall group
+	f1, err := newTestFlavor(context, 4, 4096, 8192)
+	if err != nil {
+		return nil, err
+	}
+	f2, err := newTestFlavor(context, 6, 8192, 1024)
+	if err != nil {
+		return nil, err
+	}
+	n, err := newTestNetwork(context)
+	if err != nil {
+		return nil, err
+	}
+	fw, err := newTestFirewallGroup(context)
+	if err != nil {
+		return nil, err
+	}
+
+	// Add subnet
+	s, err := newTestSubnet(context, "10.10.10.0/24", net.IPv4(10, 10, 10, 1), net.IPv4(10, 10, 10, 10), net.IPv4(10, 10, 10, 250), n)
+	if err != nil {
+		return nil, err
+	}
+
+	// Add hypervisors
+	h1, err := newTestHypervisor(context, "de:ad:be:ef:7f:21", net.IPv4(192, 168, 100, 200), net.IPv4(192, 168, 100, 1), net.IPv4(255, 255, 255, 0), "br0", s)
+	if err != nil {
+		return nil, err
+	}
+	data.hypervisors[h1.ID] = &TestingHypervisorData{"DE:AD:BE:EF:7F:21", "192.168.100.200", "192.168.100.1", "255.255.255.0"}
+	h2, err := newTestHypervisor(context, "de:ad:be:ef:7f:23", net.IPv4(192, 168, 100, 203), net.IPv4(192, 168, 100, 1), net.IPv4(255, 255, 255, 0), "br0", s)
+	if err != nil {
+		return nil, err
+	}
+	data.hypervisors[h2.ID] = &TestingHypervisorData{"DE:AD:BE:EF:7F:23", "192.168.100.203", "192.168.100.1", "255.255.255.0"}
+
+	// Add guests
+	g1, err := newTestGuest(context, "01:23:45:67:89:ab", n, s, f1, fw, h1)
+	if err != nil {
+		return nil, err
+	}
+	data.guests[g1.ID] = &TestingGuestData{"01:23:45:67:89:AB", g1.IP.String(), "10.10.10.1", "10.10.10.0"}
+	g2, err := newTestGuest(context, "23:45:67:89:ab:cd", n, s, f2, fw, h1)
+	if err != nil {
+		return nil, err
+	}
+	data.guests[g2.ID] = &TestingGuestData{"23:45:67:89:AB:CD", g2.IP.String(), "10.10.10.1", "10.10.10.0"}
+	g3, err := newTestGuest(context, "45:67:89:ab:cd:ef", n, s, f1, fw, h2)
+	if err != nil {
+		return nil, err
+	}
+	data.guests[g3.ID] = &TestingGuestData{"45:67:89:AB:CD:EF", g3.IP.String(), "10.10.10.1", "10.10.10.0"}
+	g4, err := newTestGuest(context, "67:89:ab:cd:ef:01", n, s, f2, fw, h2)
+	if err != nil {
+		return nil, err
+	}
+	data.guests[g4.ID] = &TestingGuestData{"67:89:AB:CD:EF:01", g4.IP.String(), "10.10.10.1", "10.10.10.0"}
+
+	return data, nil
+}
+
+func newTestFlavor(context *lochness.Context, cpu uint32, memory, disk uint64) (*lochness.Flavor, error) {
+	f := context.NewFlavor()
+	f.CPU = cpu
+	f.Memory = memory
+	f.Disk = disk
+	err := f.Save()
+	if err != nil {
+		return nil, errors.New("Could not save flavor: " + err.Error())
+	}
+	return f, nil
+}
+
+func newTestNetwork(context *lochness.Context) (*lochness.Network, error) {
+	n := context.NewNetwork()
+	if err := n.Save(); err != nil {
+		return nil, errors.New("Could not save network: " + err.Error())
+	}
+	return n, nil
+}
+
+func newTestFirewallGroup(context *lochness.Context) (*lochness.FWGroup, error) {
+	fw := context.NewFWGroup()
+	fw.Rules = append(fw.Rules, &lochness.FWRule{})
+	if err := fw.Save(); err != nil {
+		return nil, errors.New("Could not save firewall group: " + err.Error())
+	}
+	return fw, nil
+}
+
+func newTestHypervisor(context *lochness.Context, mac string, ip, gateway, netmask net.IP, ifname string, s *lochness.Subnet) (*lochness.Hypervisor, error) {
+	var err error
+	h := context.NewHypervisor()
+	h.IP = ip
+	h.MAC, err = net.ParseMAC(mac)
+	if err != nil {
+		return nil, errors.New("Could not parse hypervisor MAC '" + mac + "': " + err.Error())
+	}
+	h.Gateway = gateway
+	h.Netmask = netmask
+	if err = h.Save(); err != nil {
+		return nil, errors.New("Could not save hypervisor: " + err.Error())
+	}
+	if err := h.AddSubnet(s, ifname); err != nil {
+		return nil, errors.New("Could not add subnet to hypervisor: " + err.Error())
+	}
+	return h, nil
+}
+
+func newTestGuest(context *lochness.Context, mac string, n *lochness.Network, s *lochness.Subnet, f *lochness.Flavor, fw *lochness.FWGroup, h *lochness.Hypervisor) (*lochness.Guest, error) {
+	var err error
+	g := context.NewGuest()
+	g.MAC, err = net.ParseMAC(mac)
+	if err != nil {
+		return nil, errors.New("Could not parse guest MAC '" + mac + "': " + err.Error())
+	}
+	g.NetworkID = n.ID
+	g.SubnetID = s.ID
+	g.FlavorID = f.ID
+	g.FWGroupID = fw.ID
+	if err := g.Save(); err != nil {
+		return nil, errors.New("Could not save guest: " + err.Error())
+	}
+
+	if err := h.AddGuest(g); err != nil {
+		return nil, errors.New("Could not add guest to hypervisor: " + err.Error())
+	}
+
+	g.IP, err = s.ReserveAddress(g.ID)
+	if err != nil {
+		return nil, errors.New("Could not reserve guest address in subnet: " + err.Error())
+	}
+
+	if err := g.Save(); err != nil {
+		return nil, errors.New("Could not resave guest: " + err.Error())
+	}
+
+	return g, nil
+}
+
+func newTestSubnet(context *lochness.Context, cidr string, gateway, start, end net.IP, n *lochness.Network) (*lochness.Subnet, error) {
+	var err error
+	s := context.NewSubnet()
+	_, s.CIDR, err = net.ParseCIDR(cidr)
+	if err != nil {
+		return nil, errors.New("Could not parse subnet CIDR '" + cidr + "': " + err.Error())
+	}
+	s.Gateway = gateway
+	s.StartRange = start
+	s.EndRange = end
+	if err := s.Save(); err != nil {
+		return nil, errors.New("Could not save subnet: " + err.Error())
+	}
+	if err := n.AddSubnet(s); err != nil {
+		return nil, errors.New("Could not add subnet to network: " + err.Error())
+	}
+	return s, nil
+}
+
+func getLines(t *testing.T, b *bytes.Buffer) []string {
+	s := bufio.NewScanner(b)
+	re := regexp.MustCompile("\\s+")
+	var lines []string
+	for s.Scan() {
+		line := s.Text()
+		if line != "" {
+			cleaned := re.ReplaceAllString(strings.TrimSpace(line), " ")
+			lines = append(lines, cleaned)
+		}
+	}
+	h.Ok(t, s.Err())
+	return lines
+}
+
+func TestHypervisorsConf(t *testing.T) {
+
+	// Setup
+	r := refresher.NewRefresher("example.com", "http://127.0.0.1:4001")
+	h.Equals(t, r.Domain, "example.com")
+	testData, err := doTestSetup(r.Context, r.EtcdClient)
+	h.Ok(t, err)
+
+	// Write to buffer
+	b := new(bytes.Buffer)
+	err = r.WriteHypervisorsConfigFile(b)
+	h.Ok(t, err)
+
+	// Define tests and regexes
+	fkeys := []string{
+		"group start",
+		"domain",
+		"if iPXE",
+		"filename for iPXE",
+		"next-server",
+		"filename for non-iPXE",
+		"first hypervisor start",
+		"first hypervisor mac",
+		"first hypervisor ip",
+		"first hypervisor gateway",
+		"first hypervisor netmask",
+		"first hypervisor end",
+		"second hypervisor start",
+		"second hypervisor mac",
+		"second hypervisor ip",
+		"second hypervisor gateway",
+		"second hypervisor netmask",
+		"second hypervisor end",
+		"group end",
+	}
+	found := make(map[string]bool)
+	for _, k := range fkeys {
+		found[k] = false
+	}
+	groupre := regexp.MustCompile("^group ([0-9a-zA-Z]+) {$")
+	hostre := regexp.MustCompile("^host ([0-9a-f\\-]+) {$")
+	ifre := regexp.MustCompile("^if .* {$")
+	errors := make([]string, 0)
+
+	// Check the returned file line by line
+	s := bufio.NewScanner(b)
+	spacere := regexp.MustCompile("\\s+")
+	ingroup := false
+	inhost := false
+	inif := false
+	hostprefix := ""
+	var hostmatch *TestingHypervisorData
+	var line string
+	for s.Scan() {
+		line = s.Text()
+		if line == "" {
+			continue
+		}
+		line = spacere.ReplaceAllString(strings.TrimSpace(line), " ")
+		t.Log(line)
+
+		// End blocks
+		if line == "}" {
+			if inif {
+				inif = false
+				t.Log(" -- CLOSE IF")
+			} else if inhost {
+				inhost = false
+				found[hostprefix+" hypervisor end"] = true
+				t.Log(" -- CLOSE " + strings.ToUpper(hostprefix) + " HYPERVISOR")
+			} else if ingroup {
+				ingroup = false
+				found["group end"] = true
+				t.Log(" -- CLOSE GROUP")
+			}
+			continue
+		}
+
+		// Start blocks
+		if ifre.FindString(line) == line {
+			inif = true
+			t.Log(" -- OPEN IF")
+		}
+		m1 := hostre.FindStringSubmatch(line)
+		if len(m1) == 2 {
+			inhost = true
+			if found["first hypervisor start"] {
+				found["second hypervisor start"] = true
+				hostprefix = "second"
+				t.Log(" -- OPEN SECOND HYPERVISOR")
+			} else {
+				found["first hypervisor start"] = true
+				hostprefix = "first"
+				t.Log(" -- OPEN FIRST HYPERVISOR")
+			}
+			id := m1[1]
+			if _, ok := testData.hypervisors[id]; !ok {
+				errors = append(errors, "Host block not matching a hypervisor")
+				hostmatch = nil
+			} else {
+				hostmatch = testData.hypervisors[id]
+			}
+			continue
+		}
+		m2 := groupre.FindStringSubmatch(line)
+		if len(m2) == 2 {
+			ingroup = true
+			if m2[1] != "hypervisors" {
+				errors = append(errors, "Group name is not hypervisors")
+			} else {
+				found["group start"] = true
+			}
+			t.Log(" -- OPEN GROUP")
+			continue
+		}
+
+		// Host lines
+		if inhost {
+			if hostmatch == nil {
+				continue
+			}
+			if line == "hardware ethernet \""+hostmatch.mac+"\";" {
+				found[hostprefix+" hypervisor mac"] = true
+			} else if line == "fixed-address \""+hostmatch.ip+"\";" {
+				found[hostprefix+" hypervisor ip"] = true
+			} else if line == "option routers \""+hostmatch.gateway+"\";" {
+				found[hostprefix+" hypervisor gateway"] = true
+			} else if line == "option subnet-mask \""+hostmatch.netmask+"\";" {
+				found[hostprefix+" hypervisor netmask"] = true
+			}
+			continue
+		}
+
+		// Group lines
+		if ingroup {
+			if line == "option domain-name \"nodes.example.com\";" {
+				found["domain"] = true
+			}
+			if line == "if exists user-class and option user-class = \"iPXE\" {" {
+				found["if iPXE"] = true
+			}
+			if line == "filename \"http://ipxe.services.example.com:8888/ipxe/${net0/ip}\";" {
+				found["filename for iPXE"] = true
+			}
+			if line == "next-server tftp.services.example.com;" {
+				found["next-server"] = true
+			}
+			if line == "filename \"undionly.kpxe\";" {
+				found["filename for non-iPXE"] = true
+			}
+		}
+	}
+	h.Ok(t, s.Err())
+
+	// Report anything not found or wrong
+	for _, key := range fkeys {
+		if !found[key] {
+			t.Error("Config file missing " + key)
+		}
+	}
+	for _, err := range errors {
+		t.Error(err)
+	}
+
+}
+
+func TestGuestsConf(t *testing.T) {
+
+	// Setup
+	r := refresher.NewRefresher("example.com", "http://127.0.0.1:4001")
+	h.Equals(t, r.Domain, "example.com")
+	testData, err := doTestSetup(r.Context, r.EtcdClient)
+	h.Ok(t, err)
+
+	// Write to buffer
+	b := new(bytes.Buffer)
+	err = r.WriteGuestsConfigFile(b)
+	h.Ok(t, err)
+
+	// Define tests and regexes
+	fkeys := []string{
+		"group start",
+		"domain",
+		"first guest start",
+		"first guest mac",
+		"first guest ip",
+		"first guest gateway",
+		"first guest cidr",
+		"first guest end",
+		"second guest start",
+		"second guest mac",
+		"second guest ip",
+		"second guest gateway",
+		"second guest cidr",
+		"second guest end",
+		"third guest start",
+		"third guest mac",
+		"third guest ip",
+		"third guest gateway",
+		"third guest cidr",
+		"third guest end",
+		"fourth guest start",
+		"fourth guest mac",
+		"fourth guest ip",
+		"fourth guest gateway",
+		"fourth guest cidr",
+		"fourth guest end",
+		"group end",
+	}
+	found := make(map[string]bool)
+	for _, k := range fkeys {
+		found[k] = false
+	}
+	groupre := regexp.MustCompile("^group ([0-9a-zA-Z]+) {$")
+	hostre := regexp.MustCompile("^host ([0-9a-f\\-]+) {$")
+	ifre := regexp.MustCompile("^if .* {$")
+	errors := make([]string, 0)
+
+	// Check the returned file line by line
+	s := bufio.NewScanner(b)
+	spacere := regexp.MustCompile("\\s+")
+	ingroup := false
+	inhost := false
+	inif := false
+	hostprefix := ""
+	var hostmatch *TestingGuestData
+	var line string
+	for s.Scan() {
+		line = s.Text()
+		if line == "" {
+			continue
+		}
+		line = spacere.ReplaceAllString(strings.TrimSpace(line), " ")
+		t.Log(line)
+
+		// End blocks
+		if line == "}" {
+			if inif {
+				inif = false
+				t.Log(" -- CLOSE IF")
+			} else if inhost {
+				inhost = false
+				found[hostprefix+" guest end"] = true
+				t.Log(" -- CLOSE " + strings.ToUpper(hostprefix) + " GUEST")
+			} else if ingroup {
+				ingroup = false
+				found["group end"] = true
+				t.Log(" -- CLOSE GROUP")
+			}
+			continue
+		}
+
+		// Start blocks
+		if ifre.FindString(line) == line {
+			inif = true
+			t.Log(" -- OPEN IF")
+		}
+		m1 := hostre.FindStringSubmatch(line)
+		if len(m1) == 2 {
+			inhost = true
+			if found["third guest start"] {
+				found["fourth guest start"] = true
+				hostprefix = "fourth"
+				t.Log(" -- OPEN FOURTH GUEST")
+			} else if found["second guest start"] {
+				found["third guest start"] = true
+				hostprefix = "third"
+				t.Log(" -- OPEN THIRD GUEST")
+			} else if found["first guest start"] {
+				found["second guest start"] = true
+				hostprefix = "second"
+				t.Log(" -- OPEN SECOND GUEST")
+			} else if found["fourth guest start"] {
+				hostprefix = "extra"
+				t.Log(" -- OPEN EXTRA GUEST")
+			} else {
+				found["first guest start"] = true
+				hostprefix = "first"
+				t.Log(" -- OPEN FIRST GUEST")
+			}
+			id := m1[1]
+			if _, ok := testData.guests[id]; !ok {
+				errors = append(errors, "Host block not matching a guest")
+				hostmatch = nil
+			} else {
+				hostmatch = testData.guests[id]
+			}
+			continue
+		}
+		m2 := groupre.FindStringSubmatch(line)
+		if len(m2) == 2 {
+			ingroup = true
+			if m2[1] != "guests" {
+				errors = append(errors, "Group name is not guests")
+			} else {
+				found["group start"] = true
+			}
+			t.Log(" -- OPEN GROUP")
+			continue
+		}
+
+		// Host lines
+		if inhost {
+			if hostmatch == nil {
+				continue
+			}
+			if line == "hardware ethernet \""+hostmatch.mac+"\";" {
+				found[hostprefix+" guest mac"] = true
+			} else if line == "fixed-address \""+hostmatch.ip+"\";" {
+				found[hostprefix+" guest ip"] = true
+			} else if line == "option routers \""+hostmatch.gateway+"\";" {
+				found[hostprefix+" guest gateway"] = true
+			} else if line == "option subnet-mask \""+hostmatch.cidr+"\";" {
+				found[hostprefix+" guest cidr"] = true
+			}
+			continue
+		}
+
+		// Group lines
+		if ingroup {
+			if line == "option domain-name \"guests.example.com\";" {
+				found["domain"] = true
+			}
+		}
+	}
+	h.Ok(t, s.Err())
+
+	// Report anything not found or wrong
+	for _, key := range fkeys {
+		if !found[key] {
+			t.Error("Config file missing " + key)
+		}
+	}
+	for _, err := range errors {
+		t.Error(err)
+	}
+
+}
