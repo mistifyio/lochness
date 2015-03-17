7eeb18c8add6c7c601565dbcb4abc9c1c19abf16
Reha Sterbin <reha@omniti.com>
2015-03-17 20:36:50 +0000

    [MIST-370] Golint fixes

--- cmd/dobharchu/testhelper/testhelper.go
+++ cmd/dobharchu/testhelper/testhelper.go
@@ -9,6 +9,7 @@ import (
 	"github.com/mistifyio/lochness"
 )
 
+// NewTestFlavor uses the lochness context and the given values to build a new flavor
 func NewTestFlavor(context *lochness.Context, cpu uint32, memory, disk uint64) (*lochness.Flavor, error) {
 	f := context.NewFlavor()
 	f.CPU = cpu
@@ -21,6 +22,7 @@ func NewTestFlavor(context *lochness.Context, cpu uint32, memory, disk uint64) (
 	return f, nil
 }
 
+// NewTestNetwork uses the lochness context and the given values to build a new network
 func NewTestNetwork(context *lochness.Context) (*lochness.Network, error) {
 	n := context.NewNetwork()
 	if err := n.Save(); err != nil {
@@ -29,6 +31,7 @@ func NewTestNetwork(context *lochness.Context) (*lochness.Network, error) {
 	return n, nil
 }
 
+// NewTestFirewallGroup uses the lochness context and the given values to build a new firewall group
 func NewTestFirewallGroup(context *lochness.Context) (*lochness.FWGroup, error) {
 	fw := context.NewFWGroup()
 	fw.Rules = append(fw.Rules, &lochness.FWRule{})
@@ -38,6 +41,27 @@ func NewTestFirewallGroup(context *lochness.Context) (*lochness.FWGroup, error) 
 	return fw, nil
 }
 
+// NewTestSubnet uses the lochness context and the given values to build a new subnet
+func NewTestSubnet(context *lochness.Context, cidr string, gateway, start, end net.IP, n *lochness.Network) (*lochness.Subnet, error) {
+	var err error
+	s := context.NewSubnet()
+	_, s.CIDR, err = net.ParseCIDR(cidr)
+	if err != nil {
+		return nil, errors.New("Could not parse subnet CIDR '" + cidr + "': " + err.Error())
+	}
+	s.Gateway = gateway
+	s.StartRange = start
+	s.EndRange = end
+	if err := s.Save(); err != nil {
+		return nil, errors.New("Could not save subnet: " + err.Error())
+	}
+	if err := n.AddSubnet(s); err != nil {
+		return nil, errors.New("Could not add subnet to network: " + err.Error())
+	}
+	return s, nil
+}
+
+// NewTestHypervisor uses the lochness context and the given values to build a new hypervisor
 func NewTestHypervisor(context *lochness.Context, mac string, ip, gateway, netmask net.IP, ifname string, s *lochness.Subnet) (*lochness.Hypervisor, error) {
 	var err error
 	h := context.NewHypervisor()
@@ -57,6 +81,7 @@ func NewTestHypervisor(context *lochness.Context, mac string, ip, gateway, netma
 	return h, nil
 }
 
+// NewTestGuest uses the lochness context and the given values to build a new guest
 func NewTestGuest(context *lochness.Context, mac string, n *lochness.Network, s *lochness.Subnet, f *lochness.Flavor, fw *lochness.FWGroup, h *lochness.Hypervisor) (*lochness.Guest, error) {
 	var err error
 	g := context.NewGuest()
@@ -88,28 +113,10 @@ func NewTestGuest(context *lochness.Context, mac string, n *lochness.Network, s 
 	return g, nil
 }
 
-func NewTestSubnet(context *lochness.Context, cidr string, gateway, start, end net.IP, n *lochness.Network) (*lochness.Subnet, error) {
-	var err error
-	s := context.NewSubnet()
-	_, s.CIDR, err = net.ParseCIDR(cidr)
-	if err != nil {
-		return nil, errors.New("Could not parse subnet CIDR '" + cidr + "': " + err.Error())
-	}
-	s.Gateway = gateway
-	s.StartRange = start
-	s.EndRange = end
-	if err := s.Save(); err != nil {
-		return nil, errors.New("Could not save subnet: " + err.Error())
-	}
-	if err := n.AddSubnet(s); err != nil {
-		return nil, errors.New("Could not add subnet to network: " + err.Error())
-	}
-	return s, nil
-}
-
+// Cleanup removes a list of objects that have been created during the test
 func Cleanup(e *etcd.Client, created map[string]string) []error {
 	var path string
-	errs := make([]error, 0)
+	var errs []error
 	for id, t := range created {
 		switch {
 		case t == "flavor":
