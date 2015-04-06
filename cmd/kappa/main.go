35a008917503e864877e869f1b8c303b93de1e55
Reha Sterbin <reha@omniti.com>
2015-04-06 20:59:26 +0000

    [MIST-378] First pass: viper + pflag, with adjusted config

--- cmd/kappa/main.go
+++ cmd/kappa/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -15,50 +12,29 @@ import (
 	"github.com/coreos/go-etcd/etcd"
 	"github.com/mistifyio/lochness/pkg/watcher"
 	logx "github.com/mistifyio/mistify-logrus-ext"
-	flag "github.com/ogier/pflag"
+	flag "github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
 type (
 	// Tags is a list of ansible tags
 	Tags []string
 
-	// Config is a map of etcd prefixes to watch to ansible tags to run
-	Config map[string]Tags
+	// TagMap is a map of etcd prefixes to watch to ansible tags to run
+	TagMap map[string]Tags
 )
 
-var ansibleDir = "/root/lochness-ansible"
-var config Config
-
-// loadConfig reads the config file and unmarshals it into a map containing
-// prefixs to watch and ansible tags to run. An empty tag array means a full
-// playbook run. The config file should not be empty
-func loadConfig(path string) (Config, error) {
-	data, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	config := make(Config)
-	if err := json.Unmarshal(data, &config); err != nil {
-		return nil, err
-	}
-
-	if len(config) == 0 {
-		return nil, errors.New("empty config")
-	}
-
-	return config, nil
-}
+var tagMap TagMap
 
 // getTags returns the ansible tags, if any, associated with a key
 func getTags(key string) []string {
 	// Check for exact match
-	if tags, ok := config[key]; ok {
+	if tags, ok := tagMap[key]; ok {
 		return tags
 	}
 
 	// Find prefix
-	for watchPrefix, tags := range config {
+	for watchPrefix, tags := range tagMap {
 		if !strings.HasPrefix(key, watchPrefix) {
 			continue
 		}
@@ -75,6 +51,7 @@ func runAnsible(key string) {
 	for _, tag := range keyTags {
 		args = append(args, "-t", tag)
 	}
+	ansibleDir := viper.GetString("ansible")
 	cmd := exec.Command(path.Join(ansibleDir, "run"), args...)
 	cmd.Dir = ansibleDir
 	cmd.Stdout = os.Stdout
@@ -118,7 +95,7 @@ func watchKeys(etcdClient *etcd.Client) *watcher.Watcher {
 	}
 
 	// start watching etcd prefixs
-	for prefix := range config {
+	for prefix := range tagMap {
 		if err := w.Add(prefix); err != nil {
 			log.WithFields(log.Fields{
 				"prefix":   prefix,
@@ -132,60 +109,85 @@ func watchKeys(etcdClient *etcd.Client) *watcher.Watcher {
 }
 
 func main() {
-	logLevel := flag.StringP("log-level", "l", "warn", "log level")
-	flag.StringVarP(&ansibleDir, "ansible", "a", ansibleDir, "directory containing the ansible run command")
-	eaddr := flag.StringP("etcd", "e", "http://127.0.0.1:4001", "address of etcd server")
-	configPath := flag.StringP("config", "c", "", "path to config file with prefixs")
+
+	// Flags for direct use
+	var logLevel, configPath string
+	flag.StringVarP(&logLevel, "log-level", "l", "warning", "log level: debug/info/warning/error/critical/fatal")
+	flag.StringVarP(&configPath, "config", "c", "", "path to config file with prefixes")
+
+	// Flags for use with viper
+	_ = flag.StringP("etcd", "e", "http://127.0.0.1:4001", "address of etcd server")
+	_ = flag.StringP("ansible", "a", "/root/lochness-ansible", "directory containing the ansible run command")
 	flag.Parse()
 
 	// Set up logging
-	if err := logx.DefaultSetup(*logLevel); err != nil {
+	if err := logx.DefaultSetup(logLevel); err != nil {
 		log.WithFields(log.Fields{
 			"error": err,
 			"func":  "logx.DefaultSetup",
-			"level": *logLevel,
+			"level": logLevel,
 		}).Fatal("failed to set up logging")
 	}
 
-	// Load config containing prefixs to watch
-	config, err := loadConfig(*configPath)
-	if err != nil {
+	// Read in the config file
+	viper.SetConfigFile(configPath)
+	if err := viper.ReadInConfig(); err != nil {
 		log.WithFields(log.Fields{
 			"error":      err,
-			"configPath": *configPath,
+			"func":       "viper.ReadInConfig",
+			"configPath": configPath,
 		}).Fatal("failed to load config")
 	}
 
-	log.WithField("config", config).Info("config loaded")
-
-	// set up etcd connection
-	log.WithField("address", *eaddr).Info("connection to etcd")
-	etcdClient := etcd.NewClient([]string{*eaddr})
-	// make sure we can actually connect to etcd
-	if !etcdClient.SyncCluster() {
+	// Bind the flags to viper
+	viper.BindPFlags(flag.CommandLine)
+	if err := viper.ReadInConfig(); err != nil {
 		log.WithFields(log.Fields{
-			"error":   err,
-			"address": *eaddr,
-		}).Fatal("failed to connect to etcd cluster")
+			"error": err,
+			"func":  "viper.BindPFlags",
+		}).Fatal("failed to bind flags")
 	}
 
-	// set up watcher
+	// Parse the tag map
+	if err := viper.MarshalKey("tag-map", &tagMap); err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+			"func":  "viper.MarshalKey",
+			"key":   "tag-map",
+		}).Fatal("ansible tag map is improperly formed")
+	}
+	if len(tagMap) == 0 {
+		log.Fatal("ansible tag map is empty")
+	}
+	log.WithField("tag-map", tagMap).Info("ansible tag map loaded")
+
+	// Set up etcd connection
+	eaddr := viper.GetString("etcd")
+	log.WithField("address", eaddr).Info("connection to etcd")
+	etcdClient := etcd.NewClient([]string{eaddr})
+
+	// Make sure we can actually connect to etcd
+	if !etcdClient.SyncCluster() {
+		log.WithField("address", eaddr).Fatal("failed to connect to etcd cluster")
+	}
+
+	// Set up watcher
 	w := watchKeys(etcdClient)
 
-	// to coordinate clean exiting between the consumer and the signal handler
+	// To coordinate clean exiting between the consumer and the signal handler
 	ready := make(chan struct{}, 1)
 	ready <- struct{}{}
 
-	// handle events
+	// Handle events
 	go consumeResponses(w, ready)
 
-	// handle signals for clean shutdown
+	// Handle signals for clean shutdown
 	sigs := make(chan os.Signal)
 	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
 
 	s := <-sigs
 	log.WithField("signal", s).Info("signal received. waiting for current task to process")
-	// wait until any current processing is finished
+	// Wait until any current processing is finished
 	<-ready
 	_ = w.Close()
 	log.Info("exiting")
