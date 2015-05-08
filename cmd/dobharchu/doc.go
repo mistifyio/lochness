c7eb2af364c0e560a723d5e19230725df5fd70a4
Manuel Mendez <mmendez@omniti.com>
2015-05-08 17:50:51 -0400

    dobharchu: update docs

--- cmd/dobharchu/doc.go
+++ cmd/dobharchu/doc.go
@@ -7,11 +7,10 @@ The following arguments are accepted:
 
 	$ dobharchu -h
 	Usage of dobharchu:
-	-d, --domain="": domain for lochness; required
-	-e, --etcd="http://127.0.0.1:4001": address of etcd server
-		--guests-path="/etc/dhcpd/guests.conf": alternative path to guests.conf
-		--hypervisors-path="/etc/dhcpd/hypervisors.conf": alternative path to hypervisors.conf
-	-l, --log-level="warning": log level: debug/info/warning/error/critical/fatal
+	  -c, --conf-dir="/etc/dhcp/": dhcpd configuration directory
+	  -d, --domain="": domain for lochness; required
+	  -e, --etcd="http://127.0.0.1:4001": address of etcd server
+	  -l, --log-level="warning": log level: debug/info/warning/error/critical/fatal
 
 Watched
 
