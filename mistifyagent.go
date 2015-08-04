b1d8f252d22bf4ab79abcda4fcd2ad6dc00b8034
Brian Bickerton <bbickerton@omniti.com>
2015-08-05 01:29:42 +1000

    Use eth0 instead of mist1 for default nic name

--- mistifyagent.go
+++ mistifyagent.go
@@ -60,7 +60,7 @@ func (agent *MistifyAgent) generateClientGuest(g *Guest) (*client.Guest, error) 
 	}
 
 	nic := client.Nic{
-		Name:    "mist1",
+		Name:    "eth0",
 		Network: g.Bridge,
 		Model:   "virtio", // TODO: Check whether this is alwalys the case
 		Mac:     g.MAC.String(),
