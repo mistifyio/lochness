9cc05c3d51b43956c4484d297c29d1a9a8538b8b
Brian Bickerton <brian.bickerton@gmail.com>
2015-08-22 06:06:53 +1000

    VLAN and VLANGroup structs

---
+++ vlan.go
@@ -0,0 +1,154 @@
+package lochness
+
+import (
+	"encoding/json"
+	"errors"
+	"path/filepath"
+
+	"github.com/coreos/go-etcd/etcd"
+)
+
+var (
+	// VLANPath is the path in the config store for VLANs
+	VLANPath = "/lochness/vlans/"
+)
+
+type (
+	// VLAN devines the virtual lan for a guest interface
+	VLAN struct {
+		context       *Context
+		modifiedIndex uint64
+		Tag           int    `json:"tag"`
+		Description   string `json:"description"`
+		vlanGroups    []string
+	}
+
+	// VLANs is an alias to a slice of *VLAN
+	VLANs []*VLAN
+)
+
+func (c *Context) blankVLAN(tag int) *VLAN {
+	if tag == 0 {
+		tag = 1
+	}
+
+	v := &VLAN{
+		context:    c,
+		Tag:        tag,
+		vlanGroups: make([]string, 0, 0),
+	}
+
+	return v
+}
+
+// NewVLAN creates a new blank VLAN.
+func (c *Context) NewVLAN() *VLAN {
+	return c.blankVLAN(0)
+}
+
+// key is a helper to generate the config store key.
+func (v *VLAN) key() string {
+	return filepath.Join(VLANPath, string(v.Tag), "metadata")
+}
+
+func (v *VLAN) vlanGroupKey(vlanGroup *VLANGroup) string {
+	var key string
+	if vlanGroup != nil {
+		key = vlanGroup.ID
+	}
+	return filepath.Join(VLANPath, string(v.Tag), "vlangroups", key)
+}
+
+// VLAN fetches a VLAN from the data store.
+func (c *Context) VLAN(tag int) (*VLAN, error) {
+	v := c.blankVLAN(tag)
+	if err := v.Refresh(); err != nil {
+		return nil, err
+	}
+	return v, nil
+}
+
+// Refresh reloads the VLAN from the data store.
+func (v *VLAN) Refresh() error {
+	resp, err := v.context.etcd.Get(filepath.Dir(v.key()), false, false)
+	if err != nil {
+		return err
+	}
+
+	for _, node := range resp.Node.Nodes {
+		key := filepath.Base(node.Key)
+		switch key {
+		case "metadata":
+			if err := json.Unmarshal([]byte(node.Value), &v); err != nil {
+				return err
+			}
+			v.modifiedIndex = node.ModifiedIndex
+		case "vlangroups":
+			for _, x := range node.Nodes {
+				v.vlanGroups = append(v.vlanGroups, x.Key)
+			}
+		}
+	}
+
+	return nil
+}
+
+// Validate ensures a VLAN has resonable data.
+func (v *VLAN) Validate() error {
+	if v.Tag <= 0 {
+		return errors.New("invalid tag")
+	}
+	return nil
+}
+
+// Save persists a VLAN. It will call Validate.
+func (v *VLAN) Save() error {
+	if err := v.Validate(); err != nil {
+		return err
+	}
+
+	value, err := json.Marshal(v)
+	if err != nil {
+		return err
+	}
+
+	// if something changed, don't clobber
+	var resp *etcd.Response
+	if v.modifiedIndex != 0 {
+		resp, err = v.context.etcd.CompareAndSwap(v.key(), string(value), 0, "", v.modifiedIndex)
+	} else {
+		resp, err = v.context.etcd.Create(v.key(), string(value), 0)
+	}
+	if err != nil {
+		return err
+	}
+
+	v.modifiedIndex = resp.EtcdIndex
+
+	return nil
+}
+
+// Destroy removes the VLAN
+func (v *VLAN) Destroy() error {
+	// Unlink VLANGroups
+	for _, vlanGroupID := range v.vlanGroups {
+		vlanGroup, err := v.context.VLANGroup(vlanGroupID)
+		if err != nil {
+			return err
+		}
+		if err := vlanGroup.RemoveVLAN(v); err != nil {
+			return err
+		}
+	}
+
+	// Delete the VLAN
+	if _, err := v.context.etcd.Delete(filepath.Dir(v.key()), true); err != nil {
+		return err
+	}
+	return nil
+}
+
+// VLANGroups returns the IDs of the VLANGroups associated with the VLAN
+func (v *VLAN) VLANGroups() []string {
+	return v.vlanGroups
+}
---
+++ vlangroup.go
@@ -0,0 +1,212 @@
+package lochness
+
+import (
+	"encoding/json"
+	"errors"
+	"path/filepath"
+	"strconv"
+
+	"code.google.com/p/go-uuid/uuid"
+
+	"github.com/coreos/go-etcd/etcd"
+)
+
+var (
+	// VLANGroupPath is the path in the config store for VLAN groups
+	VLANGroupPath = "/lochness/vlangroups/"
+)
+
+type (
+	// VLANGroup defines a set of VLANs for a guest interface
+	VLANGroup struct {
+		context       *Context
+		modifiedIndex uint64
+		ID            string            `json:"id"`
+		Description   string            `json:"description"`
+		Metadata      map[string]string `json:"metadata"`
+		vlans         []int
+	}
+
+	// VLANGroups is an alias to a slice of *VLANGroup
+	VLANGroups []*VLANGroup
+)
+
+func (c *Context) blankVLANGroup(id string) *VLANGroup {
+	vg := &VLANGroup{
+		context:  c,
+		ID:       id,
+		Metadata: make(map[string]string),
+		vlans:    make([]int, 0, 0),
+	}
+
+	if id == "" {
+		vg.ID = uuid.New()
+	}
+
+	return vg
+}
+
+// key is a helper to generate the config store key.
+func (vg *VLANGroup) key() string {
+	return filepath.Join(VLANGroupPath, vg.ID, "metadata")
+}
+
+func (vg *VLANGroup) vlanKey(vlan *VLAN) string {
+	var key int
+	if vlan != nil {
+		key = vlan.Tag
+	}
+	return filepath.Join(VLANGroupPath, vg.ID, "vlans", string(key))
+}
+
+// NewVLANGroup creates a new blank VLANGroup.
+func (c *Context) NewVLANGroup() *VLANGroup {
+	return c.blankVLANGroup("")
+}
+
+// VLANGroup fetches a VLAN from the data store.
+func (c *Context) VLANGroup(id string) (*VLANGroup, error) {
+	var err error
+	id, err = canonicalizeUUID(id)
+	if err != nil {
+		return nil, err
+	}
+	vg := c.blankVLANGroup(id)
+	if err = vg.Refresh(); err != nil {
+		return nil, err
+	}
+	return vg, nil
+}
+
+// Refresh reloads the VLAN from the data store.
+func (vg *VLANGroup) Refresh() error {
+	resp, err := vg.context.etcd.Get(filepath.Dir(vg.key()), false, false)
+	if err != nil {
+		return err
+	}
+
+	for _, node := range resp.Node.Nodes {
+		key := filepath.Base(node.Key)
+		switch key {
+		case "metadata":
+			if err := json.Unmarshal([]byte(node.Value), &vg); err != nil {
+				return err
+			}
+			vg.modifiedIndex = node.ModifiedIndex
+		case "vlans":
+			for _, x := range node.Nodes {
+				vlanTag, _ := strconv.Atoi(filepath.Base(x.Key))
+				vg.vlans = append(vg.vlans, vlanTag)
+			}
+		}
+	}
+
+	return nil
+}
+
+// Validate ensures a VLANGroup has resonable data.
+func (vg *VLANGroup) Validate() error {
+	if _, err := canonicalizeUUID(vg.ID); err != nil {
+		return errors.New("invalid ID")
+	}
+	return nil
+}
+
+// Save persists a VLANgroup. It will call Validate.
+func (vg *VLANGroup) Save() error {
+	if err := vg.Validate(); err != nil {
+		return err
+	}
+
+	value, err := json.Marshal(vg)
+	if err != nil {
+		return err
+	}
+
+	// if something changed, don't clobber
+	var resp *etcd.Response
+	if vg.modifiedIndex != 0 {
+		resp, err = vg.context.etcd.CompareAndSwap(vg.key(), string(value), 0, "", vg.modifiedIndex)
+	} else {
+		resp, err = vg.context.etcd.Create(vg.key(), string(value), 0)
+	}
+	if err != nil {
+		return err
+	}
+
+	vg.modifiedIndex = resp.EtcdIndex
+	return nil
+}
+
+// Destroy removes a VLANGroup
+func (vg *VLANGroup) Destroy() error {
+	// Unlink VLANs
+	for _, vlanTag := range vg.vlans {
+		vlan, err := vg.context.VLAN(vlanTag)
+		if err != nil {
+			return err
+		}
+		if err := vg.RemoveVLAN(vlan); err != nil {
+			return err
+		}
+	}
+
+	// Delete the VLANGroup
+	if _, err := vg.context.etcd.Delete(filepath.Dir(vg.key()), true); err != nil {
+		return err
+	}
+	return nil
+}
+
+// AddVLAN adds a VLAN to the VLANGroup
+func (vg *VLANGroup) AddVLAN(vlan *VLAN) error {
+	// VLANGroup side
+	if _, err := vg.context.etcd.Set(vg.vlanKey(vlan), "", 0); err != nil {
+		return err
+	}
+	vg.vlans = append(vg.vlans, vlan.Tag)
+
+	// VLAN side
+	if _, err := vlan.context.etcd.Set(vlan.vlanGroupKey(vg), "", 0); err != nil {
+		return err
+	}
+	vlan.vlanGroups = append(vlan.vlanGroups, vg.ID)
+
+	return nil
+}
+
+// RemoveVLAN removes a VLAN from the VLANGroup
+func (vg *VLANGroup) RemoveVLAN(vlan *VLAN) error {
+	// VLANGroup side
+	if _, err := vg.context.etcd.Delete(vg.vlanKey(vlan), false); err != nil {
+		return err
+	}
+
+	newVLANs := make([]int, 0, len(vg.vlans)-1)
+	for _, vlanTag := range vg.vlans {
+		if vlanTag != vlan.Tag {
+			newVLANs = append(newVLANs, vlanTag)
+		}
+	}
+	vg.vlans = newVLANs
+
+	// VLAN side
+	if _, err := vlan.context.etcd.Delete(vlan.vlanGroupKey(vg), false); err != nil {
+		return err
+	}
+
+	newVLANGroups := make([]string, 0, len(vlan.vlanGroups)-1)
+	for _, vlanGroup := range vlan.vlanGroups {
+		if vlanGroup != vg.ID {
+			newVLANGroups = append(newVLANGroups, vlanGroup)
+		}
+	}
+	vlan.vlanGroups = newVLANGroups
+
+	return nil
+}
+
+// VLANs returns the Tags of the VLANs associated with the VLANGroup
+func (vg *VLANGroup) VLANs() []int {
+	return vg.vlans
+}
