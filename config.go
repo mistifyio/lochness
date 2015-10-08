3f04ef3f2c5848ea37604465e9b4395857fc9328
Brian Bickerton <brian.bickerton@gmail.com>
2015-10-09 01:22:00 +1100

    Improve config tests and behavior of config functions

--- config.go
+++ config.go
@@ -1,16 +1,26 @@
 package lochness
 
-import "path/filepath"
+import (
+	"errors"
+	"path/filepath"
+	"strings"
+
+	"github.com/coreos/go-etcd/etcd"
+)
 
 //Used to get set arbitrary config variables
 
 var (
 	// ConfigPath is the path in the config store.
-	ConfigPath = "lochness/config/"
+	ConfigPath = "/lochness/config/"
 )
 
 // GetConfig gets a single value from the config store. The key can contain slashes ("/")
 func (c *Context) GetConfig(key string) (string, error) {
+	if key == "" {
+		return "", errors.New("empty config key")
+	}
+
 	resp, err := c.etcd.Get(filepath.Join(ConfigPath, key), false, false)
 	if err != nil {
 		return "", err
@@ -22,6 +32,10 @@ func (c *Context) GetConfig(key string) (string, error) {
 
 // SetConfig sets a single value from the config store. The key can contain slashes ("/")
 func (c *Context) SetConfig(key, val string) error {
+	if key == "" {
+		return errors.New("empty config key")
+	}
+
 	_, err := c.etcd.Set(filepath.Join(ConfigPath, key), val, 0)
 	return err
 }
@@ -32,11 +46,21 @@ func (c *Context) ForEachConfig(f func(key, val string) error) error {
 	if err != nil {
 		return err
 	}
-	for _, n := range resp.Node.Nodes {
-		k := filepath.Base(n.Key)
-		v := n.Value
-		if err := f(k, v); err != nil {
-			return err
+	return forEachConfig(resp.Node.Nodes, f)
+}
+
+func forEachConfig(nodes etcd.Nodes, f func(key, val string) error) error {
+	for _, n := range nodes {
+		if n.Dir {
+			if err := forEachConfig(n.Nodes, f); err != nil {
+				return err
+			}
+		} else {
+			k := strings.TrimPrefix(n.Key, ConfigPath)
+			v := n.Value
+			if err := f(k, v); err != nil {
+				return err
+			}
 		}
 	}
 	return nil
