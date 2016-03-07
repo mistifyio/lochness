532bdf3bba13439264ab8c5d9e25c1d3bebf412c
Manuel Mendez <mmendez534@gmail.com>
2016-03-07 17:54:21 -0500

    change cdhcpd log message from sentence caps to all lowercase

--- cmd/cdhcpd/main.go
+++ cmd/cdhcpd/main.go
@@ -29,7 +29,7 @@ func updateConfigs(f *Fetcher, r *Refresher, hconfPath, gconfPath string) (bool,
 		log.WithFields(log.Fields{
 			"error": err,
 			"func":  "fetcher.Hypervisors",
-		}).Error("Could not fetch hypervisors")
+		}).Error("could not fetch hypervisors")
 		return restart, err
 	}
 
@@ -40,7 +40,7 @@ func updateConfigs(f *Fetcher, r *Refresher, hconfPath, gconfPath string) (bool,
 				"error": err,
 				"func":  "Refresher.genHypervisorsConf",
 				"type":  "hypervisors",
-			}).Error("Could not generate configuration")
+			}).Error("could not generate configuration")
 		}
 		return err
 	})
@@ -55,7 +55,7 @@ func updateConfigs(f *Fetcher, r *Refresher, hconfPath, gconfPath string) (bool,
 		log.WithFields(log.Fields{
 			"error": err,
 			"func":  "fetcher.Guests",
-		}).Error("Could not fetch guests")
+		}).Error("could not fetch guests")
 		return restart, err
 	}
 	subnets, err := f.Subnets()
@@ -63,7 +63,7 @@ func updateConfigs(f *Fetcher, r *Refresher, hconfPath, gconfPath string) (bool,
 		log.WithFields(log.Fields{
 			"error": err,
 			"func":  "fetcher.Subnets",
-		}).Error("Could not fetch subnets")
+		}).Error("could not fetch subnets")
 		return restart, err
 	}
 
@@ -74,7 +74,7 @@ func updateConfigs(f *Fetcher, r *Refresher, hconfPath, gconfPath string) (bool,
 				"error": err,
 				"func":  "Refresher.genGuestsConf",
 				"type":  "guests",
-			}).Error("Could not generate configuration")
+			}).Error("could not generate configuration")
 		}
 		return err
 	})
@@ -106,7 +106,7 @@ func writeConfig(confType, path string, checksum []byte, generator func(io.Write
 			"error": err,
 			"path":  tmp,
 			"type":  confType,
-		}).Error("Could not generate temporary configuration")
+		}).Error("could not generate temporary configuration")
 		return nil, err
 	}
 
@@ -116,7 +116,7 @@ func writeConfig(confType, path string, checksum []byte, generator func(io.Write
 			"func":  "buff.Flush",
 			"path":  tmp,
 			"type":  confType,
-		}).Error("Could not flush buffer to temporary conf file")
+		}).Error("could not flush buffer to temporary conf file")
 		return nil, err
 	}
 
@@ -126,7 +126,7 @@ func writeConfig(confType, path string, checksum []byte, generator func(io.Write
 			"func":  "os.File.Close",
 			"path":  tmp,
 			"type":  confType,
-		}).Error("Could not close temporary conf file")
+		}).Error("could not close temporary conf file")
 		return nil, err
 	}
 
@@ -148,14 +148,14 @@ func writeConfig(confType, path string, checksum []byte, generator func(io.Write
 			"from":  tmp,
 			"to":    path,
 			"type":  confType,
-		}).Error("Could not rename temporary conf file")
+		}).Error("could not rename temporary conf file")
 		return nil, err
 	}
 
 	log.WithFields(log.Fields{
 		"path": path,
 		"type": confType,
-	}).Info("Replaced conf file")
+	}).Info("replaced conf file")
 
 	return hash.Sum(nil), nil
 }
@@ -193,7 +193,7 @@ func main() {
 		log.WithFields(log.Fields{
 			"error": err,
 			"func":  "logx.DefaultSetup",
-		}).Fatal("Could not set up logrus")
+		}).Fatal("could not set up logrus")
 	}
 
 	hconfPath := path.Join(confPath, "hypervisors.conf")
@@ -222,7 +222,7 @@ func main() {
 		log.WithFields(log.Fields{
 			"error": err,
 			"func":  "watcher.New",
-		}).Fatal("Could not create watcher")
+		}).Fatal("could not create watcher")
 	}
 
 	// Start watching the necessary etcd prefixs
@@ -233,7 +233,7 @@ func main() {
 				"error":  err,
 				"func":   "watcher.Add",
 				"prefix": prefix,
-			}).Fatal("Could not add watch prefix")
+			}).Fatal("could not add watch prefix")
 		}
 	}
 
@@ -249,7 +249,7 @@ func main() {
 		// Integrate the response and update the configs if necessary
 		refresh, err := f.IntegrateResponse(w.Response())
 		if err != nil {
-			log.Info("Error on integration; re-fetching")
+			log.Info("error on integration; re-fetching")
 			err := f.FetchAll()
 			if err != nil {
 				os.Exit(1)
@@ -265,7 +265,7 @@ func main() {
 				log.WithFields(log.Fields{
 					"error": err,
 					"func":  "updateConfigs",
-				}).Warn("Could not create watcher")
+				}).Warn("could not create watcher")
 			}
 		}
 
@@ -273,7 +273,7 @@ func main() {
 		ready <- done
 	}
 	if err := w.Err(); err != nil {
-		log.WithField("error", err).Fatal("Watcher encountered an error")
+		log.WithField("error", err).Fatal("watcher encountered an error")
 	}
 
 	// Handle signals for clean shutdown
@@ -281,8 +281,8 @@ func main() {
 	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
 
 	s := <-sigs
-	log.WithField("signal", s).Info("Signal received; waiting for current task to process")
+	log.WithField("signal", s).Info("signal received; waiting for current task to process")
 	<-ready // wait until any current processing is finished
 	_ = w.Close()
-	log.Info("Exiting")
+	log.Info("exiting")
 }
